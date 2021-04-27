0cf337cbf0c1fe0f41c4f2b08259d59a4ac86912
mamihagimoto <mamihagi0404@gmail.com>
2021-04-27 14:09:33 +0900

    test

--- js/js/jspractice.js
+++ js/js/jspractice.js
@@ -1,6 +1,40 @@
 // document.write("Hello World!");
 // alert("Hello World!");
 
-console.log("Hellp World!"+"田中さん");
-console.log(10 + 10);
-console.log("10"+"10");
+// console.log("Hellp World!"+"田中さん");
+// console.log(10 + 10);
+// console.log("10"+"10");
+
+// let x=1;
+// let y=1;
+// x++;
+// console.log(x);
+// y--;
+// console.log(y);
+
+// console.log(1+1);
+// console.log(1-1);
+// console.log(2*2);
+// console.log(10/3);
+// console.log(10%3);
+
+// let x=7;
+// let y=x++;
+// let x2=7;
+// let y2=++x2;
+
+// console.log(x);
+// console.log(y);
+// console.log(x2);
+// console.log(y2);
+
+'use strict';
+let age = 30;
+const name = "田中太郎";
+age= age+2;
+
+console.log(`${name}さんの年齢は${age}です`);
+
+// var 変数　定数は使わない
+// ES 2015版
+// let　より const　を優先して使用する
